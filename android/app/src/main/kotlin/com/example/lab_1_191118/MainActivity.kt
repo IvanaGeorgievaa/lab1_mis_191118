b98ce84090840b32c1940e90e3743b120f67b1d6
Ivana Georgieva <ivanag@vestelglobal.com>
2022-11-11 18:32:17 +0100

    lab 1 files

---
+++ android/app/src/main/kotlin/com/example/lab_1_191118/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab_1_191118
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lab_1_191118/home.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
